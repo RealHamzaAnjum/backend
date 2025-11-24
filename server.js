41b97ba79d2eaf400ed558e0cc95cf0d73e96bee
Hamza Sameer Anjum <148954760+RealHamzaAnjum@users.noreply.github.com>
2025-11-24 23:58:05 +0500

    Modify CORS settings and methods in server.js
    Updated CORS origins and added HEAD method support.

--- server.js
+++ server.js
@@ -45,9 +45,9 @@ app.use(cors({
     'http://localhost:4000',
     'http://localhost:5174',
     'http://localhost:5173',
-    'https://buildestate.vercel.app',
-    'https://real-estate-website-admin.onrender.com',
-    'https://real-estate-website-backend-zfu7.onrender.com',
+    'https://frontend-realestate-zfiv.vercel.app',
+    'https://admin-kohl-nine.vercel.app',
+    'https://real-estate-backend-production-d02b.up.railway.app',
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'], // Added HEAD
